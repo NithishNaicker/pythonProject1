86d06f5b5f0d4c108814f70bf8f0d3ca54f0bfec
NithishNaicker <86906013+NithishNaicker@users.noreply.github.com>
2023-05-18 12:14:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,request,jsonify
 import numpy as np
 import pickle
 
+
+
 model = pickle.load(open('model.pkl','rb'))
 
 app = Flask(__name__)
@@ -10,17 +12,18 @@ app = Flask(__name__)
 def index():
     return "Hello world"
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['GET'])
 def predict():
-    cgpa = request.form.get('cgpa')
-    iq = request.form.get('iq')
-    profile_score = request.form.get('profile_score')
+    Symptom_1 = request.form.get('Symptom_1')
+    Symptom_2 = request.form.get('Symptom_2')
+    Symptom_3 = request.form.get('Symptom_3')
 
-    input_query = np.array([[cgpa,iq,profile_score]])
 
-    result = model.predict(input_query)[0]
 
-    return jsonify({'placement':str(result)})
+
+    result = model.predict([[int(Symptom_1),int(Symptom_2),int(Symptom_3)]])
+
+    return str(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
