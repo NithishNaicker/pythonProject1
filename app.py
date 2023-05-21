c1d840da1f3cb55fe866ec820f10e436a2106caf
NithishNaicker <86906013+NithishNaicker@users.noreply.github.com>
2023-05-21 23:59:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def index():
     return "Hello world"
 
-@app.route('/predict',methods=['GET'])
+@app.route('/predict',methods=['POST'])
 def predict():
     Symptom_1 = request.form.get('Symptom_1')
     Symptom_2 = request.form.get('Symptom_2')
@@ -21,9 +21,9 @@ def predict():
 
 
 
-    result = model.predict([[int(Symptom_1),int(Symptom_2),int(Symptom_3)]])
+    result = model.predict([[(Symptom_1),(Symptom_2),(Symptom_3)]])
 
-    return jsonify({'disease':str(result)})
+    return jsonify({'disease': str(result)})
 
 if __name__ == '__main__':
     app.run(debug=True)
